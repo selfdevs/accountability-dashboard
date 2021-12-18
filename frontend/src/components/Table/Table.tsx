6c38db8336db597c5065dbc9caa4fc291f9e741d
cchampou <clement@champouillon.com>
2021-12-18 19:17:39 +0100

    perf: memo data for the table

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import './styles.css';
 import { sampleDataGenerator } from '../../mocks/table-data';
 import TableHeader from './TableHeader';
 import TableRow from './TableRow';
 
 const Table = () => {
-  const data = sampleDataGenerator();
+  const data = useMemo(sampleDataGenerator, []);
 
   return (
     <table className="data-table">
