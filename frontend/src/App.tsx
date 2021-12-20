5a6b5fd281dfea31d5c2fbc70f54969c2fc073c8
Clément Champouillon <clement@champouillon.com>
2021-12-20 18:19:13 +0100

    :sparkles: Add a router for the application (#22)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter, NavLink, Route, Routes,
+} from 'react-router-dom';
 import ScratchPad from './components/ScratchPad/ScrathPad';
 import Table from './components/Table/Table';
 
 const App = () => (
-  <div className="layout">
-    <Table />
-    <div>
-      <ScratchPad />
-    </div>
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/:username"
+        element={(
+          <div className="layout">
+            <Table />
+            <div>
+              <ScratchPad />
+            </div>
+          </div>
+        )}
+      />
+      <Route index element={<NavLink to="/longlikeshort">Go to longlikeshort dashboard</NavLink>} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
