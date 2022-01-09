3b9e0aa7cc6ffbdd5bc104256f7d995c1c5aa340
cchampou <clement@champouillon.com>
2022-01-09 04:57:30 +0100

    with env files

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -4,7 +4,9 @@ import { request } from '../modules/http/client';
 import { saveToStorage } from '../modules/storage/io';
 import LoginButton from '../components/LoginButton/LoginButton';
 
-const LOGIN_URL = 'https://discord.com/api/oauth2/authorize?client_id=927637722628259842&redirect_uri=https%3A%2F%2Fbig-donkey-12.loca.lt%2F&response_type=code&scope=identify%20email';
+const LOGIN_URL = `https://discord.com/api/oauth2/authorize?client_id=927637722628259842&redirect_uri=${encodeURIComponent(
+  process.env.REACT_APP_REDIRECT_URI
+)}&response_type=code&scope=identify%20email`;
 
 const Homepage = () => {
   const [params] = useSearchParams();
@@ -13,18 +15,20 @@ const Homepage = () => {
   useEffect(() => {
     const code = params.get('code');
     if (code) {
-      request('/auth/discord', 'POST', { code })
-        .then(({ token }: { token: string }) => {
+      request('/auth/discord', 'POST', { code }).then(
+        ({ token }: { token: string }) => {
           saveToStorage('accessToken', token);
           navigate('/me');
-        });
+        }
+      );
     }
   }, [params, navigate]);
 
   return (
-    <LoginButton onClick={() => {
-      window.location.href = LOGIN_URL;
-    }}
+    <LoginButton
+      onClick={() => {
+        window.location.href = LOGIN_URL;
+      }}
     />
   );
 };
