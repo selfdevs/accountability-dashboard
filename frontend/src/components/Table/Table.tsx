018098eb490c8118092c3d1de6fb58b2eaf9f9d8
cchampou <clement@champouillon.com>
2022-01-09 21:58:27 +0100

    fix layout again

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -16,18 +16,20 @@ const Table: FC<TableProps> = ({ data, className }) => {
   const { tbodyRef } = useScrollToCurrentDay(data.length > 0);
 
   return (
-    <div
-      className={`table-wrapper ${className || ''}`}
-      style={{ position: 'relative', flex: '1' }}
-    >
-      <table className="data-table">
-        <TableHeader />
-        <tbody ref={tbodyRef}>
-          {data.map((entry) => (
-            <TableRow {...entry} key={entry.date} setEditId={setEditId} />
-          ))}
-        </tbody>
-      </table>
+    <div className="left-column-table">
+      <div
+        className={`table-wrapper ${className || ''}`}
+        style={{ position: 'relative', flex: '1' }}
+      >
+        <table className="data-table">
+          <TableHeader />
+          <tbody ref={tbodyRef}>
+            {data.map((entry) => (
+              <TableRow {...entry} key={entry.date} setEditId={setEditId} />
+            ))}
+          </tbody>
+        </table>
+      </div>
       <EntryEditor entries={data} editId={editId} setEditId={setEditId} />
     </div>
   );
