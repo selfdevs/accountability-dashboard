d6794edd0e78a0fc65f8524603431eca9c1dbac4
cchampou <clement@champouillon.com>
2022-01-09 05:12:59 +0100

    fix api base url

--- frontend/src/modules/http/client.ts
+++ frontend/src/modules/http/client.ts
@@ -2,10 +2,11 @@ import { getFromStorage } from '../storage/io';
 
 export const request = async (path: string, method?: string, data?: object) => {
   const token = getFromStorage('accessToken');
-  const response = await fetch(`http://localhost:5000/api${path}`, {
+  const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}${path}`, {
     method: method || 'get',
     headers: {
-      'Content-Type': 'application/json', ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      'Content-Type': 'application/json',
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
     },
     ...(data ? { body: JSON.stringify(data) } : {}),
   });
