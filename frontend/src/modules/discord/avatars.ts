c467368b8bde7f1d7b003d31a3248d1173b38548
cchampou <clement@champouillon.com>
2022-01-10 17:43:18 +0100

    refactor: name condition

--- frontend/src/modules/discord/avatars.ts
+++ frontend/src/modules/discord/avatars.ts
@@ -4,12 +4,14 @@ export const generateAvatarUrl = (
   discordId: User['discordId'],
   discordAvatar: User['discordAvatar']
 ): string => {
-  if (
+  const isMissingAvatarData =
     !discordAvatar ||
     discordAvatar.length === 0 ||
     !discordId ||
-    discordId.length === 0
-  )
+    discordId.length === 0;
+
+  if (isMissingAvatarData)
     return 'https://icon-library.com/images/no-profile-picture-icon/no-profile-picture-icon-15.jpg';
+
   return `${process.env.REACT_APP_DISCORD_CDN}/avatars/${discordId}/${discordAvatar}.png`;
 };
