537602677b21c0198cf5b115484b6fa3de147ccc
cchampou <clement@champouillon.com>
2022-01-17 22:37:45 +0100

    wip

---
+++ backend/src/services/__tests__/authService.spec.ts
@@ -0,0 +1,24 @@
+import { generateJWT, getTokenFromAuthorizationHeader } from '../authService';
+import { sign } from 'jsonwebtoken';
+
+jest.mock('jsonwebtoken', () => ({
+  sign: jest.fn(),
+}));
+
+process.env.AUTH_SECRET = 'secret';
+process.env.TOKEN_LIFESPAN = '3600';
+
+describe('getTokenFromAuthorizationHeader', () => {
+  it('should return the token from the authorization header', () => {
+    expect(getTokenFromAuthorizationHeader('Bearer token')).toBe('token');
+  });
+});
+
+describe('generateJWT', () => {
+  it('should return a JWT', () => {
+    generateJWT('userId');
+    expect(sign).toHaveBeenCalledWith({ userId: 'userId' }, 'secret', {
+      expiresIn: 3600,
+    });
+  });
+});
